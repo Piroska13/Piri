c400bff0406dd18a23fc182e1460bfbd095a1480
Piroska13 <markopirike@gmail.com>
2016-10-18 20:56:38 +0200

    Add files via upload

---
+++ game2.py
@@ -0,0 +1,30 @@
+
+
+
+
+
+def key_press():
+    direction=input("Direction: ")
+    if direction == "w":
+        up_nums()
+    if direction == "s":
+        down_nums()
+    if direction == "a":
+        left_nums()
+    if direction == "d":
+        right_nums()
+
+def key_press(event):
+def key_press(self, event):
+    direction=input("Direction: ")
+    if direction == 'KP_DUp'.keysym
+        up_nums()
+    if direction == 'KP_Down'.keysym
+        down_nums()
+    if direction == 'KP_Left':.keysym
+        left_nums()
+    if direction == "d":'KP_Right'.keysym
+        right_nums()
+
+def Startposition():
+    
---
+++ game3.py
@@ -0,0 +1,11 @@
+import random
+Stage=[[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+
+
+def PlayGround():
+    print (Stage[0][0], "\t", Stage[0][1], "\t", Stage[0][2], "\t", Stage[0][3])
+    print (Stage[1][0], "\t", Stage[1][1], "\t", Stage[1][2], "\t", Stage[1][3])
+    print (Stage[2][0], "\t", Stage[2][1], "\t", Stage[2][2], "\t", Stage[2][3])
+    print (Stage[3][0], "\t", Stage[3][1], "\t", Stage[3][2], "\t", Stage[3][3])
+
+PlayGround()
